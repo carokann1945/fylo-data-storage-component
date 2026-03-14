16351bb33bdddc9e96e0ad106602255cb5f11a73
carokann1945 <carokann1945@gmail.com>
2026-03-15 03:54:09 +0900

    initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import { Raleway } from 'next/font/google';
+import './globals.css';
+
+const raleway = Raleway({
+  subsets: ['latin'],
+  variable: '--font-raleway',
+});
+
+export const metadata: Metadata = {
+  title: 'fylo-data-storage-component',
+  description: 'frontend mentor challenge',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${raleway.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
